a2ee16104fd80361a56aea08842b4f22b815cadd
tau <fernando.canteruccio@gmail.com>
2017-10-28 17:32:33 -0200

    add print running test
    Signed-off-by: tau <fernando.canteruccio@gmail.com>

--- cryptotrader/models/cn_models.py
+++ cryptotrader/models/cn_models.py
@@ -254,6 +254,7 @@ def train_nn(nn, env, test_env, optimizer, batch_size, train_epochs, test_interv
             if epoch % test_interval == 0 and epoch != 0:
                 test_losses = []
                 test_scores = []
+                print("Running test epochs...")
                 for j in range(test_epochs):
                     test_batch, target_test = make_batch(test_env, batch_size)
 
@@ -273,7 +274,7 @@ def train_nn(nn, env, test_env, optimizer, batch_size, train_epochs, test_interv
                     test_loss_log.append(np.mean(test_losses))
                     test_r2_log.append(np.mean(test_scores))
 
-                    print("\nTest epoch: %d, loss: %f, r2: %f" % (j + 1, loss_test.data, test_r2.data))
+                    print("Test epoch: %d, loss: %f, r2: %f" % (j + 1, loss_test.data, test_r2.data), end='\r')
 
                 if np.mean(test_scores) > best_score:
                     best_score = np.mean(test_scores)
